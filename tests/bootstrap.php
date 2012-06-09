444100bcf6d98e4ab077e843a5d64dfdf2b0a20c
Christoph <christoph@webvariants.de>
2012-06-09 02:42:50 +0200

    clone SallyCMS 0.2 as a test-repo

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2012, Christoph Mewes, http://www.xrstf.de/
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+define('LIBHG_BASE',  rtrim(realpath(dirname(__FILE__).'/../'), DIRECTORY_SEPARATOR));
+define('LIBHG_REPOS', LIBHG_BASE.DIRECTORY_SEPARATOR.'repos');
+
+require LIBHG_BASE.'/Autoload.php';
