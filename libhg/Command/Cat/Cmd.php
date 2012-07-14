f857f47a307ff4e883df981d619e81a9b90e3228
Christoph <christoph@webvariants.de>
2012-07-14 06:49:15 +0200

    avoid the overhead of encoding and decoding the own class members

--- libhg/Command/Cat/Cmd.php
+++ libhg/Command/Cat/Cmd.php
@@ -28,14 +28,12 @@ class libhg_Command_Cat_Cmd extends libhg_Command_Cat_Base {
 	 * @return libhg_Command_Cat_Result
 	 */
 	public function evaluate(libhg_Stream_Readable $reader, libhg_Stream_Writable $writer, libhg_Repository_Interface $repo) {
-		$text    = trim($reader->readString(libhg_Stream::CHANNEL_OUTPUT));
-		$code    = $reader->readReturnValue();
-		$options = $this->getCommandOptions();
-		$output  = $options->getSingle('-o');
+		$text = trim($reader->readString(libhg_Stream::CHANNEL_OUTPUT));
+		$code = $reader->readReturnValue();
 
 		// output was redirected into a file
-		if ($output !== null) {
-			return new libhg_Command_Cat_Result($output, true, $code);
+		if ($this->output !== null) {
+			return new libhg_Command_Cat_Result($this->output, true, $code);
 		}
 
 		return new libhg_Command_Cat_Result($text, false, $code);
